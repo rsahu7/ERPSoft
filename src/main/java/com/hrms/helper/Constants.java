1181823fdbcbb83716609beaf1c529ff48756096
rsahu7 <sahu.roshan730@gmail.com>
2017-09-10 21:43:06 +0530

    removed src

--- src/main/java/com/hrms/helper/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.hrms.helper;
-
-public class Constants {
-
-	// All constants string should be listed here..
-	public static String SUPPLIER_PREFIX_CODE = "SUP";
-	public static String CUSTOMER_PREFIX_CODE = "CUS";
-	public static String PROJECT_PREFIX_CODE = "PRJ";
-}
